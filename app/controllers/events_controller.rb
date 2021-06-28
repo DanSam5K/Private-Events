b259d0390edba419373178544459a8f2a71efa8d
Daniel Samuel <dansam5000@gmail.com>
2021-06-28 23:29:33 +0100

    Update events routes and controller to allow one create events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,28 @@
 class EventsController < ApplicationController
+  include EventsHelper
+  before_action :authenticate_user!
+
   def index
     @events = Event.all
+  end
+
+  def show
+    @event = Event.find(params[:id])
+  end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = current_user.events.build(event_params)
+
+    if @event.save
+      flash[:success] = "Event '#{@event.name}' created!"
+      redirect_to @event
+    else
+      flash[:alert] = "Some error!"
+      render 'new'
+    end
   end
 end
--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,2 +1,6 @@
 module EventsHelper
+  def event_params
+    params.require(:event).permit(:name, :date, :location, :description, :user_id)
+  end
 end
+  
