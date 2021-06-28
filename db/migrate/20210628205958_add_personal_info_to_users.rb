37921b53bf0aad55c286a11fc2059de60b389fbe
Daniel Samuel <dansam5000@gmail.com>
2021-06-28 23:16:09 +0100

    Add fields to db/schema and migrate to db

---
+++ db/migrate/20210628205958_add_personal_info_to_users.rb
@@ -0,0 +1,5 @@
+class AddPersonalInfoToUsers < ActiveRecord::Migration[6.1]
+  def change
+    add_column :users, :name, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_27_204140) do
+ActiveRecord::Schema.define(version: 2021_06_28_205958) do
 
   create_table "events", force: :cascade do |t|
     t.string "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2021_06_27_204140) do
     t.string "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -29,6 +30,7 @@ ActiveRecord::Schema.define(version: 2021_06_27_204140) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
