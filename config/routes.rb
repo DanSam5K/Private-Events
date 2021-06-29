a3e993d91e97cdd0c4551ec42f32fa4eb6437d4d
Daniel Samuel <dansam5000@gmail.com>
2021-06-29 09:06:50 +0100

    Updates routes with enrollment paths

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   root "events#index"
 
   resources :events
+  resources :enrollments, only: [:create, :destroy, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
