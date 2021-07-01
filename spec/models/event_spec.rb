c906709c46c78d72679ae78ff41ebdb4c8f96dd5
Daniel Samuel <dansam5000@gmail.com>
2021-07-01 06:23:40 +0100

    Fix linters errors

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,11 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Event, type: :model do
-  context "with 2 or more Events" do
-    it "orders them in reverse past and future events" do
+  context 'with 2 or more Events' do
+    it 'orders them in reverse past and future events' do
       event = Event.create!
-      event1 = event.create!(:name => "first event")
-      event2 = event.create!(:name => "second event")
+      event1 = event.create!(name: 'first event')
+      event2 = event.create!(name: 'second event')
       expect(event.reload.events).to eq([event1, event2])
     end
   end
