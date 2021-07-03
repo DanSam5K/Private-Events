fbe3f340263b974c60803dc061e4496dd33e6ab2
Daniel Samuel <dansam5000@gmail.com>
2021-07-03 11:54:29 +0100

    Add route to Guest List

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   resources :events
   resources :enrollments
   get 'user/show', action: :show, controller: 'users' 
+  get 'user/index', action: :index, controller: 'users'
   resources :enrollments, only: [:create, :destroy, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
