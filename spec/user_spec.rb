c78b7df31f5c77de21b902afd00428aa127be9ee
Daniel Samuel <dansam5000@gmail.com>
2021-07-05 18:22:37 +0100

    Test email and password

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -12,4 +12,22 @@ RSpec.describe User, type: :model do
       expect(user.macro).to eq(:has_many)
     end
   end
+
+  describe 'email' do
+    it 'must be present' do
+      user = described_class.new(name: 'John', email: 'john@example.com', password: '123456')
+      expect(user).to be_valid
+      user.email = nil
+      expect(user).to_not be_valid
+    end
+  end
+
+  describe 'password' do
+    it 'must be present' do
+      user = described_class.new(name: 'John', email: 'john@example.com', password: '123456')
+      expect(user).to be_valid
+      user.password = nil
+      expect(user).to_not be_valid
+    end
+  end
 end
